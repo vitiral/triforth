5962efd9f705103632fbb937ec6600fac3040d1d
Rett Berg <rett@google.com>
2020-10-08 14:57:10 -0600

    expand simplemath

--- tests/simplemath.fs
+++ tests/simplemath.fs
@@ -2,3 +2,20 @@
 
 : simpleMathAnswer 0x 42 ;
 simpleMathAnswer 0x 42 assertEq
+
+: double dup + ;
+: quadruple double double ;
+
+0x 0 double 0x 0 assertEq
+0x 12 double 0x 24 assertEq
+0x 4 quadruple 0x 10 assertEq
+
+\ Division
+0x A 0x 3 /mod   \ 10 / 3 -> ( quotient remainder )
+  0x 1 assertEq  \ remainder
+  0x 3 assertEq  \ quotient
+
+\ Multiplication
+0x 10 0x 10 * 0x 100 assertEq
+
+assertEmpty
