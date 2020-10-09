496300788454e4670d6cfe285569108fd46f9509
Rett Berg <rett@google.com>
2020-10-09 10:40:10 -0600

    actually fix IMM

--- triforth.fs
+++ triforth.fs
@@ -4,12 +4,12 @@
 \ Please note that we don't have ( comments ) until we define them,
 \ so we will use line comments for everything.
 
+&state @ 0x 0 assertEq assertEmpty  \ test: we are in runmode
 : /       /mod swap drop ; \ ( a b -- a/b )
 : mod     /mod drop ;   \ ( a b -- a%b )
 : '\n'    0x A ;
-'\n' 0x A assertEq assertEmpty
-state @ 0x 0 assertEq
-
+'\n' 0x A assertEq assertEmpty      \ test: '\n' works
+&state @ 0x 0 assertEq assertEmpty  \ test: we are in runmode
 
 : bl      0x 20 ;       \ BL (Blank) is std forth word for space
 : cr      '\n' emit ;   \ print carriage return a.k.a newline
@@ -17,12 +17,18 @@ state @ 0x 0 assertEq
 : negate  0x 0 SWAP - ; \ get negative of the number
 : true    0x FFFFFFFF ;
 : false   0x 0 ;
+LATEST nt>xt dumpInfo drop LATEST 0x 48 dump
 : LITERAL IMM  \ ( u -- ) take whatever is on stack and compile as a literal
-  ' LIT , \ compile LIT
+  ' LIT , \ compile xt of LIT
   , ;     \ compile the literal itself (from the stack)
-\ : ':' \ ( -- c:colon ) the colon character code
-\   [ CHAR : ]  \ put the character ':' onto the stack at compile-time
-\   LITERAL ;   \ compile LIT 58 as the definition for ':' word
+: ':' \ ( -- c) the colon character code
+  [ CHAR : ]  \ put the character ':' onto the stack at compile-time
+  LITERAL ;   \ compile LIT 58 as the definition for ':' word
+assertEmpty
+\ &state @ 0x 0 assertEq          \ test: we are in runmode
+\ ':' 0x 3A assertEq assertEmpty  \ test: ':' works, therefore LITERAL, etc works
+
+
 \ : ';' [ CHAR ; ] LITERAL ; \ ;
 \ : '(' [ CHAR ( ] LITERAL ; \ (
 \ : ')' [ CHAR ) ] LITERAL ; \ )
