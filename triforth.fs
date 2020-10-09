51219b882cc1abf932d6224ea0b83de96d51329b
Rett Berg <rett@google.com>
2020-10-09 11:36:55 -0600

    add [COMPILE] and LITBYTES

--- triforth.fs
+++ triforth.fs
@@ -4,12 +4,12 @@
 \ Please note that we don't have ( comments ) until we define them,
 \ so we will use line comments for everything.
 
-&state @ 0x 0 assertEq assertEmpty  \ test: we are in runmode
+&state @ 0x 0 assertEq assertEmpty  \ test: we are in runmode, nostack
 : /       /mod swap drop ; \ ( a b -- a/b )
 : mod     /mod drop ;   \ ( a b -- a%b )
 : '\n'    0x A ;
-'\n' 0x A assertEq assertEmpty      \ test: '\n' works
-&state @ 0x 0 assertEq assertEmpty  \ test: we are in runmode
+'\n' 0x A assertEq assertEmpty      \ test: '\n' works, nostack
+&state @ 0x 0 assertEq assertEmpty  \ test: we are in runmode, nostack
 
 : bl      0x 20 ;       \ BL (Blank) is std forth word for space
 : cr      '\n' emit ;   \ print carriage return a.k.a newline
@@ -24,22 +24,21 @@
 : ':' \ ( -- c) the colon character code
   [ CHAR : ]  \ put the character ':' onto the stack at compile-time
   LITERAL ;   \ compile LIT 58 as the definition for ':' word
-assertEmpty
-\ &state @ 0x 0 assertEq          \ test: we are in runmode
-\ ':' 0x 3A assertEq assertEmpty  \ test: ':' works, therefore LITERAL, etc works
+&state @ 0x 0 assertEq          \ test: we are in runmode
+':' 0x 3A assertEq assertEmpty  \ test: ':' works, therefore LITERAL, etc works
+
+: ';' [ CHAR ; ] LITERAL ; \ ;
+: '(' [ CHAR ( ] LITERAL ; \ (
+: ')' [ CHAR ) ] LITERAL ; \ )
+: '"' [ CHAR " ] LITERAL ; \ "
+: 'A' [ CHAR A ] LITERAL ; \ A
+: '0' [ CHAR 0 ] LITERAL ; \ 0
+: '-' [ CHAR - ] LITERAL ; \ -
+: '.' [ CHAR . ] LITERAL ; \ .
+';' 0x 3B assertEq  '(' 0x 28 assertEq  ')' 0x 29 assertEq \ test: yo'basic
+
+: [COMPILE] IMM \ ( -- ) immediately compile the next word
+  word find nt>xt \ get xt for next word
+  , ;             \ and compile it
 
 
-\ : ';' [ CHAR ; ] LITERAL ; \ ;
-\ : '(' [ CHAR ( ] LITERAL ; \ (
-\ : ')' [ CHAR ) ] LITERAL ; \ )
-\ : '"' [ CHAR " ] LITERAL ; \ "
-\ : 'A' [ CHAR A ] LITERAL ; \ A
-\ : '0' [ CHAR 0 ] LITERAL ; \ 0
-\ : '-' [ CHAR - ] LITERAL ; \ -
-\ : '.' [ CHAR . ] LITERAL ; \ .
-\ 
-\ \ While compiling '[COMPILE]' compiles 'word' if it would otherwise be IMM
-\ : [COMPILE] IMM
-\   WORD FIND NT>XT \ get xt for next word
-\   , ;             \ and compile it
-\ 
